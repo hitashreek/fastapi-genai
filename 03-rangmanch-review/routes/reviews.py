b9cb277fde40bb63332d72a1f7d2c3cd7edf1267
hitashreek <hkorgaonkar03@gmail.com>
2026-09-18 22:25:06 +0530

    review CRUD endpoints with SQLModel

--- 03-rangmanch-review/routes/reviews.py
+++ 03-rangmanch-review/routes/reviews.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlmodel import Session, select, func
 from models import Review, ReviewCreate, ReviewRead, ReviewUpdate
 from database import get_session
@@ -30,5 +30,61 @@ def list_reviews(
         
     query = query.offset(skip).limit(limit)
     
-    reviews = session.exec(query).all()
+    reviews = session.exec(query).all()  # all() Returns all matching results as a list.
     return reviews
+
+
+@router.get("/average/{play_name}")
+def get_average_rating(play_name: str, session: Session = Depends(get_session)):
+    result = session.exec(
+            select(func.avg(Review.rating), func.count(Review.id)).where(
+                Review.play_name == play_name
+            )
+    ).first()  # first() gets the first result from the query.
+    
+    avg_rating, total_reviews = result  # bcz result = (4.0, 3)
+    
+    if total_reviews == 0:
+        raise HTTPException(status_code=404, detail=f"No review found for {play_name}")
+    
+    return {
+        "play_name": play_name,
+        "average_rating": round(avg_rating, 2),
+        "total_reviews": total_reviews
+    }
+    
+
+@router.get("/{review_id}", response_model=ReviewRead)
+def get_review(review_id: int, session: Session = Depends(get_session)):
+    review = session.get(Review, review_id)
+    if not review:
+        raise HTTPException(status_code=404, detail="No Review")
+    return review
+
+@router.patch("/{review_id}", response_model=ReviewRead)
+def update_review(review_id: int, update: ReviewUpdate, session: Session = Depends(get_session)):
+    review = session.get(Review, review_id)
+    if not review:
+        raise HTTPException(status_code=404, detail="No Review")
+    
+    update_data = update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(review, key, value)  # setattr() is a Python built-in function used to set/update an attribute of an object.
+        
+    session.add(review)
+    session.commit()
+    session.refresh(review)
+    return review
+
+
+@router.delete("/{review_id}")
+def delete_review(review_id: int, session: Session = Depends(get_session)):
+    review = session.get(Review, review_id)
+    if not review:
+        raise HTTPException(status_code=404, detail="No Review")
+        
+    session.delete(review)
+    session.commit()
+    
+    return {"message": "Review deleted"}
+
